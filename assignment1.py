dc3a2020374752492b274718d242dbaa891b674e
Anastasia Atole <c00312185@setu.ie>
2025-09-29 14:53:57 +0000

    first part

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+
+def ip_parser(line):            #define a function that has line as a parameter
+    if " from " in line:        #if " from " is the line
+        parts = line.split()    #splits the line in tokens and separates by space by default
+        try:
+            anchor = parts.index("from")     #find where from is -> this is the anchor
+            ip = parts[anchor+1]            #the ip is the next token
+            return ip.strip()               #remove any punctuation
+
+        except (ValueError, IndexError):
+            return None
+        return None
