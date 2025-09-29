f7a665a2cf2deb90cfc1d9fe18b60b29f28a9bf6
Anastasia Atole <c00312185@setu.ie>
2025-09-29 15:16:18 +0000

    .

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,4 @@
-
+#part1
 def ip_parser(line):            #define a function that has line as a parameter
     if " from " in line:        #if " from " is the line
         parts = line.split()    #splits the line in tokens and separates by space by default
@@ -10,3 +10,17 @@ def ip_parser(line):            #define a function that has line as a parameter
         except (ValueError, IndexError):
             return None
         return None
+
+#part2
+
+from collections import defaultdict
+
+counts = defaultdict(int)           #create a dictionary to keep track of ips
+
+with open("sample_auth_small.log") as file:     #open and store sample_auth_small.log as file
+    for line in file:
+        if "Failed password" in line or "Invalid user" in line:     #if failed password or invalid user is in the line
+            ip = ip_parser(line)        #extract ip by calling the function ip_parser
+            if ip:                       #if the ip is the line
+                counts[ip] +=1          #get the current count of the ip and add one to it
+print(counts)
